2b93a861aa13ca646a5e95e4c943b39b7b7acf05
Brandon Henricks <brandon.henricks1984@gmail.com>
2018-05-14 15:19:54 -0400

    Placeholder for baseservice interface. Placeholder code for AnchorTextService

--- Interfaces/IBaseService.cs
+++ Interfaces/IBaseService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace MozscapeAPI.NET.Interfaces
 {
 	public interface IBaseService
 	{
+		T GetResult<T>(ApiAuthorization apiAuthorization, string targetUrl);
 	}
 }
