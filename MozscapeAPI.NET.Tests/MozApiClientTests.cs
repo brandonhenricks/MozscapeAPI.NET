f0e796c0596573a15c713e9c6ae4d5c9abd6470f
Brandon Henricks <brandon.henricks1984@gmail.com>
2018-05-15 13:20:32 -0400

    Constructor Tests Added

--- MozscapeAPI.NET.Tests/MozApiClientTests.cs
+++ MozscapeAPI.NET.Tests/MozApiClientTests.cs
@@ -1,15 +1,33 @@
 using System;
 using NUnit.Framework;
+using MozscapeAPI.NET.Interfaces;
+using NSubstitute.Exceptions;
+using NSubstitute;
+using System.Xml.Linq;
 
 namespace MozscapeAPI.NET.Tests
 {
 	[TestFixture]
 	public class MozApiClientTests
 	{
-		[Test]
-		public void Test1()
-		{
+		private IMozApiClient _mozApiclient;
 
+		[SetUp]
+		public void Init()
+		{
+			_mozApiclient = Substitute.For<IMozApiClient>();
+		}
+
+		[Test]
+		public void MozApiClient_Null_Constructor_Arguments_Throws_Exception()
+		{
+			Assert.Throws<ArgumentNullException>(() => new MozApiClient(null, null));
+		}
+
+		[Test]
+		public void MozApiClient_Constructor_Null_ApiAuthorization_Throws_Exception()
+		{
+			Assert.Throws<ArgumentNullException>(() => new MozApiClient(null, "https://test.com"));
 		}
 	}
 }
