f43371550aa880cf50c4ee230096a052bc3841ff
Brandon Henricks <brandon.henricks1984@gmail.com>
2018-05-23 11:51:57 -0400

    Constants Update

---
+++ MozscapeAPI.NET/Constants/EndPointConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MozscapeAPI.NET.Constants
+{
+	public static class EndPointConstants
+	{
+		public static string BASE_ENDPOINT = "http://lsapi.seomoz.com/linkscape/";
+		public static string URL_METRICS = "url-metrics/";
+		public static string TOP_PAGE = "top-pages/";
+		public static string ANCHOR_TEXT = "anchor-text/";
+		public static string LINK_SCAPE = "links/";
+	}
+}
